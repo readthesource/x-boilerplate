aa62b3b43e2fd8b116237fc6d60d6f0318a1d777
Erik Isaksen <nevraeka@gmail.com>
2013-12-06 00:39:32 -0500

    [concat] Added Grunt Concat Task

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,24 @@ module.exports = function(grunt) {
           base: "app"
         }
       }
+    },
+    concat: {
+      dest: {
+        files: {
+          'generated-javascript.js': [
+            'js/**/*.js',
+            'js/app.js'
+          ],                                                                                        'generated-styles.css': [
+            'css/**/*.css',
+            'css/styles.css'
+          ]
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   return grunt.registerTask('default', [ "connect" ]);
 };
